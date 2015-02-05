8e97f6cf902762e1f7dda8c7351e2bfd3bcba3ce
Elihu Garrett <pmaekb@hotmail.com>
2015-02-04 19:55:40 -0600

    Update and rename midimoon.lua to midmoon.lua

--- midmoon.lua
+++ midmoon.lua
@@ -1,30 +1,35 @@
-midi = require "luamidi"
-moonlet = require "moonlet"
+--Midi module
+--Still on development
+
+local midi = require "luamidi"
+local moont = require "moonlet"
+
+local midmoon = {}
 
 local noteOn, noteOff = midi.noteOn, midi.noteOff
 
-function midiPlay(port,note,vel,ch,dur)
+function midmoon.play_midi(port,note,vel,ch,dur)
   port = port or 0
   ch = ch or 1
   vel = vel or 70
   dur = dur or 0
   noteOn(port,note,vel,ch)
-  sleep(dur)
+  moont.sleep(dur)
   noteOff(port,note,vel,ch)
 end
 
-function mpattern(patrones,temp,vol1,vol2,dis,pit,ch,vel)
+function midmoon.mpattern(patrones,temp,vol1,vol2,dis,pit,ch,vel)
   pit = pit or 0.5
   dis = dis or 0
   for i, v in ipairs(patrones) do
     if type(bank[v]) == "function" then bank[v](temp)
-      elseif type(bank[v]) == "nil" then midiPlay(1,v,vel,ch)
+      elseif type(bank[v]) == "nil" then midmoon.play_midi(0,v,vel,ch)
       else soundPlay(bank[v],vol1,vol2,dis,pit)
     end  
   end
 end
 
-function midiseq(arg)
+function midmoon.seq_midi(arg)
 --[[lenght, every, variationPattern,shift1,dinoVar,tempo,volL,volR,loop--]]
   arg.speed = arg.speed or 120 --tempo
   arg.speed2 = arg.speed2 or arg.speed
@@ -35,14 +40,14 @@ function midiseq(arg)
   
   for q = 0, arg.lenght do
     for i = 0, arg.every do
-      if i == arg.every then mpattern(arg.pattern2,t(arg.speed2),arg.R,arg.L,arg.disparity2,arg.pitch2,arg.channel,arg.vel2)
-      elseif i < arg.every then mpattern(arg.pattern,t(arg.speed),arg.R,arg.L,arg.disparity,arg.pitch,arg.channel,arg.vel)
+      if i == arg.every then midmoon.mpattern(arg.pattern2,moont.t(arg.speed2),arg.R,arg.L,arg.disparity2,arg.pitch2,arg.channel,arg.vel2)
+      elseif i < arg.every then midmoon.mpattern(arg.pattern,moont.t(arg.speed),arg.R,arg.L,arg.disparity,arg.pitch,arg.channel,arg.vel)
       end
     end
   end
 end
 
-function midicor(var)
+function midmoon.sec_midi(var)
   local x
   var.speed = var.speed or 120
   var.speed2 = var.speed2 or var.speed 
@@ -57,7 +62,7 @@ function midicor(var)
       dis = dis or 0
       for i, v in ipairs(patrones) do
         if type(bank[v]) == "function" then bank[v](temp)
-          elseif type(bank[v]) == "nil" then midiPlay(1,v,vel,ch)
+          elseif type(bank[v]) == "nil" then midmoon.play_midi(0,v,vel,ch)
           else soundPlay(bank[v],vol1,vol2,dis,pit)
         end
         yield()
@@ -71,7 +76,7 @@ function midicor(var)
       dis = dis or 0
       for i, v in ipairs(patrones) do
         if type(bank[v]) == "function" then bank[v](temp)
-          elseif type(bank[v]) == "nil" then midiPlay(1,v,vel,ch)
+          elseif type(bank[v]) == "nil" then midmoon.play_midi(0,v,vel,ch)
           else soundPlay(bank[v],vol1,vol2,dis,pit)
         end
         yield()
@@ -81,7 +86,9 @@ function midicor(var)
 )
   if #var.pattern >= #var.pattern2 then x = #var.pattern else x = #var.pattern2 end
   for i = 1,x do
-    par(var.pattern,t(var.speed),var.R,var.L,var.disparity,var.pitch,var.channel,var.vel)
-    arp(var.pattern2,t(var.speed2),var.R2,var.L2,var.disparity2,var.pitch2,var.channel,var.vel2)
+    par(var.pattern,moont.t(var.speed),var.R,var.L,var.disparity,var.pitch,var.channel,var.vel)
+    arp(var.pattern2,moont.t(var.speed2),var.R2,var.L2,var.disparity2,var.pitch2,var.channel,var.vel2)
   end
 end
+
+return midmoon
