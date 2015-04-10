1fd7590466e712ba93a180dba642b4b95fadb265
Elihu Garrett <pmaekb@hotmail.com>
2015-04-09 17:40:53 -0700

    Update midmoon.lua

--- midmoon.lua
+++ midmoon.lua
@@ -75,49 +75,6 @@ function midmoon.seq_midi(arg)
   end
 end
 
-function midmoon.sec_midi(var)
-  local x
-  var.speed = var.speed or 120
-  var.speed2 = var.speed2 or var.speed 
-  var.L = var.L or 0.2
-  var.L2 = var.L2 or var.L
-  var.R = var.R or 0.2
-  var.R2 = var.R2 or var.R
-  
-  local par = wrap(function (patrones,temp,vol1,vol2,dis,pit,ch,vel,dur)
-    while true do
-      pit = pit or tone
-      dis = dis or 0
-      for i, v in ipairs(patrones) do
-        if type(bank[v]) == "function" then bank[v](temp)
-          elseif type(bank[v]) == "nil" then midmoon.play_midi(1,v,vel,ch,dur)
-          else soundPlay(bank[v],vol1,vol2,dis,pit)
-        end
-        yield()
-      end    
-    end
-  end
-)
-  local arp = wrap(function (patrones,temp,vol1,vol2,dis,pit,ch,vel,dur)
-    while true do
-      pit = pit or tone
-      dis = dis or 0
-      for i, v in ipairs(patrones) do
-        if type(bank[v]) == "function" then bank[v](temp)
-          elseif type(bank[v]) == "nil" then midmoon.play_midi(1,v,vel,ch,dur)
-          else soundPlay(bank[v],vol1,vol2,dis,pit)
-        end
-        yield()
-      end    
-    end
-  end
-)
-  if #var.pattern >= #var.pattern2 then x = #var.pattern else x = #var.pattern2 end
-  for i = 1,x do
-    par(var.pattern,moont.t(var.speed),var.R,var.L,var.disparity,var.pitch,var.channel,var.vel,arg.dur)
-    arp(var.pattern2,moont.t(var.speed2),var.R2,var.L2,var.disparity2,var.pitch2,var.channel,var.vel2,arg.dur)
-  end
-end
 
 function midmoon.sek(foo)
   --list - a table
@@ -153,4 +110,37 @@ function midmoon.sek(foo)
   end
 end
 
+function midmoon.LSek(foo)
+  --list - a table
+  --velNote - velocity of midi note
+  --velChord - velocity of midi chord
+  --channelNote - channel of midi note
+  --channelChord - channel of midi chord
+  --typeChord - major or minor chord
+  foo.portNote = foo.portNote or 1
+  foo.durNote = foo.durNote or 0.5
+  foo.durChord = foo.durChord or 0.5
+  foo.portChord = foo.portChord or 1
+  foo.L = foo.L or 0.5
+  foo.R = foo.R or foo.L
+  foo.disparity = foo.disparity or 0
+  foo.pitch = foo.pitch or tone
+  
+  do
+    if type(foo.seq) == "string" and tonumber(foo.seq) ~= nil then
+      midmoon.play_midi(foo.portNote,foo.seq,foo.velNote,foo.channelNote,foo.durNote)
+    elseif type(foo.seq) == "number" then
+      midmoon.midi_chord{
+        port = foo.portChord,
+        chord = foo.typeChord,
+        note = foo.seq,
+        dur = foo.durChord,
+        channel = foo.channelChord,
+        vel = foo.velChord
+      }
+    else
+      soundPlay(bank[foo.seq],foo.L,foo.R,foo.disparity,foo.pitch)
+    end
+  end
+end
 return midmoon
