1d16a89d3e9d48dc17e5a3c9cfd772d56a54d52e
Elihu Garrett <pmaekb@hotmail.com>
2015-09-30 12:01:35 -0500

    Update moonlet.lua

--- moonlet.lua
+++ moonlet.lua
@@ -5,6 +5,7 @@
 local Gen = require "gen"
 local allen = require "allen"
 local midi = require "luamidi"
+local moses = require "moses"
 local moont = {}
 
 math.randomseed(os.clock())
@@ -20,6 +21,7 @@ local max = math.max
 local min = math.min
 local floor = math.floor
 local unpack = unpack
+local xp = xpcall
 local tono = 1 --change to 0.5 if you are on linux
 local noteOn, noteOff = midi.noteOn, midi.noteOff
 local dir = "../Samples/Techno/"
@@ -111,7 +113,7 @@ end
 
 function moont.init(file)
 	while true do
-		local luna = xpcall(function () return require(file) end,
+		local luna = xp(function () return require(file) end,
       function () print("error"); moont.sleep(1) end)
 		package.loaded.luna = nil
 	end
@@ -119,8 +121,8 @@ end
 
 function moont.normalize(rango_i, rango_f, t)
   local tr = {}
-  local x_max = max(unpack(t))
-  local x_min = min(unpack(t))
+  local x_max = max(moses.max(t))
+  local x_min = min(moses.min(t))
   for i = 1, #t do
     tr[i] = floor(rango_i + ((t[i] - x_min) * (rango_f - rango_i) / (x_max - x_min)))
   end
@@ -140,7 +142,7 @@ end
 function string:notes(t)
   local tw = {}
   local i = 1
-  for v in self:gmatch '%S+' do
+  for v in self:gmatch '.' do
     tw[i] = v:byte()
     i = i + 1
   end
@@ -293,6 +295,44 @@ function moont.m_seq(foo)
       soundPlay(bank[foo.seq],foo.L,foo.R,foo.disparity,foo.pitch)
     end
   end
+end
+
+local f = function () end
+
+function moont.n_seq(n)
+  
+  n.L1 = n.L1 or 1
+  n.R1 = n.R1 or 1
+  n.disparity1 = n.disparity1 or 0
+  n.pitch = n.pitch or 1
+  
+  n.L2 = n.L2 or n.L1
+  n.R2 = n.R2 or n.R1
+  n.disparity2 = n.disparity2 or n.disparity1
+  n.pitch2 = n.pitch2 or pitch1
+  
+  n.port1 = n.port1 or 0
+  n.vel1 = n.vel1 or 70
+  n.ch1 = n.ch1 or 1
+  
+  n.port2 = n.port2 or n.port1
+  n.vel2 = n.vel2 or n.vel1
+  n.ch2 = n.ch2 or n.ch1
+  
+  xp(function () 
+      soundPlay(bank[n.beat1],n.L1,n.R1,n.disparity1,n.pitch1)
+    end,f)
+  xp(function () 
+      soundPlay(bank[n.beat2],n.L2,n.R2,n.disparity2,n.pitch2)
+    end,f)
+  xp(function () noteOn(n.port1,notes[n.note1],n.vel1,n.ch1) end,f)
+  xp(function () noteOn(n.port2,notes[n.note2],n.vel2,n.ch2) end,f)
+  
+  moont.sleep(n.dur)
+  
+  xp(function () noteOff(n.port1,notes[n.note1],n.vel1,n.ch1) end,f)
+  xp(function () noteOff(n.port2,notes[n.note2],n.vel2,n.ch2) end,f)
+  
 end
 
 return moont
